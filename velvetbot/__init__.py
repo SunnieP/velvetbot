b0e46659c052d170178bafcf17354bef53efd6ec
SunnieP <65377315+SunnieP@users.noreply.github.com>
2025-11-27 21:15:18 -0700

    Initialize velvetbot module with basic setup

---
+++ velvetbot/__init__.py
@@ -0,0 +1,8 @@
+"""VelvetBot - All-in-One Discord Bot for Content Creators & Business"""
+
+__version__ = '1.0.0'
+__author__ = 'VelvetBot'
+
+from .bot import VelvetBot
+from .config import Config
+from .database import Database
